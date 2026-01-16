755bbe85771ec298e5b8fc47692368a3a8e9ed4b
krishivpan <103297164+krishivpan@users.noreply.github.com>
2026-01-16 16:27:48 -0500

    fixed text formatting

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,13 @@ export default function Hero() {
     return (
         <header>
             <h1>Krishiv Panchal</h1>
-            <span class="info">award-winning computer engineer interested in impact-driven projects.</span>
             <span class="info">
+                award-winning computer engineer interested in impact-driven projects. <br/>
                 as an engineer, i like to solve problems. as a changemaker i often spend my weekends <br/>
                 pushing my limits by taking on challenging projects.
             </span>
 
-            
+
         </header>
     )
 }
